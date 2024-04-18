2661933cd6052496a608319335981fc2aee828bf
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-18 05:27:09 +0000

    completed all user debugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
 
+  def home
+    redirect_to("/users")
+  end
+
   def index
     matching_users = User.all
     @users = matching_users.order(:created_at)
 
-    render({ :template => "users_templates/all_users"})
+    render({ :template => "user_templates/all_users"})
   end
   
   def show
@@ -24,13 +28,13 @@ class UsersController < ApplicationController
   end
   
   def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
+    user_username = params.fetch("username")
+    matching_users = User.where({ :username => user_username })
     the_user = matching_users.at(0)
     
     the_user.username = params.fetch("query_username")
     the_user.save
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 
 end
